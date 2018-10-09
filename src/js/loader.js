e26c4ef87e8e1f851f67ee3ab994a5331e533778
Alexander <forcewizu@gmail.com>
2018-10-09 16:39:30 +0300

    page about-us

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,18 +1,17 @@
-(function($) {
-	var i = 1;
-	$(window).on(
-		'load',
-		setTimeout(function timer() {
-			$preloader = $('.loader');
-			$loader = $preloader.find('.loader__percent');
-			if (i < 101 && document.readyState !== 'complete') {
-				$loader.text(i);
-				i++;
-				setTimeout(timer, 10);
-			} else {
-				$loader.fadeOut();
-				$preloader.delay(350).fadeOut('slow');
-			}
-		}, 10)
-	);
-})(jQuery);
+window.addEventListener('load', function() {
+    $preloader = document.querySelector('.loader');
+    $loader = document.querySelector('.loader__percent');
+    let i = 0;
+    setTimeout(function timer() {
+        if (i < 101 && document.readyState !== 'complete') {
+            $loader.innerHTML = i;
+            i++;
+            setTimeout(timer, 100);
+        } else {
+            $preloader.classList.toggle('loader__fade');
+            setTimeout(function() {
+                $preloader.hidden = true;
+            }, 400);
+        }
+    }, 10);
+});
