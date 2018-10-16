dd3c25a1c4ee43f3e8de10cb05af8f21f3c531f1
Alexander <forcewizu@gmail.com>
2018-10-17 01:57:40 +0300

    responsive

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 'use strict';
-const gulp = require('gulp'), //основной плагин gulp
-	sass = require('gulp-sass'), //препроцессор sass
-	prefixer = require('gulp-autoprefixer'), //расставление автопрефиксов
-	cssmin = require('gulp-minify-css'), //минификация css
-	uglify = require('gulp-uglify'), //минификация js
-	imagemin = require('gulp-imagemin'), //минимизация изображений
-	rimraf = require('rimraf'), //очистка
-	sourcemaps = require('gulp-sourcemaps'), //sourcemaps
-	rename = require('gulp-rename'), //переименвоание файлов
-	plumber = require('gulp-plumber'), //предохранитель для остановки гальпа
-	watch = require('gulp-watch'), //расширение возможностей watch
-	connect = require('gulp-connect'), //livereload
+const gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	prefixer = require('gulp-autoprefixer'),
+	cssmin = require('gulp-minify-css'),
+	uglify = require('gulp-uglify'),
+	imagemin = require('gulp-imagemin'),
+	rimraf = require('rimraf'),
+	sourcemaps = require('gulp-sourcemaps'),
+	rename = require('gulp-rename'),
+	plumber = require('gulp-plumber'),
+	watch = require('gulp-watch'),
+	connect = require('gulp-connect'),
 	pump = require('pump'),
 	shell = require('gulp-shell'),
 	babel = require('gulp-babel'),
@@ -19,7 +19,6 @@ const gulp = require('gulp'), //основной плагин gulp
 
 var path = {
 	build: {
-		//Тут мы укажем куда складывать готовые после сборки файлы
 		htmlMain: 'build/',
 		html: 'build/components/',
 		jsMain: 'build/js/',
@@ -31,10 +30,9 @@ var path = {
 		cssPreview: 'src/css/'
 	},
 	src: {
-		//Пути откуда брать исходники
-		htmlMain: 'src/*.html', //Синтаксис src/template/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+		htmlMain: 'src/*.html',
 		html: 'src/components/**/[^_]*.html',
-		jsMain: 'src/js/**/*.js', //В стилях и скриптах нам понадобятся только main файлы
+		jsMain: 'src/js/**/*.js',
 		js: 'src/components/**/[^_]*.js',
 		jsminMain: 'src/js/*.js',
 		jsmin: 'src/components/**/[^_]*.js',
@@ -43,7 +41,6 @@ var path = {
 		img: 'src/image/**/*.*'
 	},
 	watch: {
-		//Тут мы укажем, за изменением каких файлов мы хотим наблюдать
 		html: 'src/components/**/*.html',
 		htmlMain: 'src/**/*.html',
 		jsMain: 'src/js/**/*.js',
@@ -53,113 +50,102 @@ var path = {
 		img: 'src/image/**/*.*',
 		fonts: 'src/fonts/**/*.*'
 	},
-	clean: './build', //директории которые могут очищаться
+	clean: './build',
 	cleanSrcCss: './src/css',
-	outputDir: './src' //исходная корневая директория для запуска минисервера
+	outputDir: './src'
 };
 
-gulp.task('serve', ['sass-only'], function() {
+gulp.task('serve', ['sass'], function() {
 	browserSync.init({
 		server: './src/'
 	});
 
-	gulp.watch('src/**/*.scss', ['sass', browserSync.reload]);
+	gulp.watch('src/**/*.*', ['sass', browserSync.reload]);
 	gulp.watch('src/**/*.html').on('change', browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass-only', function() {
+gulp.task('sass-prefixer', function() {
 	return gulp
 		.src('src/scss/*.scss')
 		.pipe(sass())
+		.prefixer({
+			browsers: ['last 2 versions']
+		})
 		.pipe(gulp.dest('src/css'))
 		.pipe(browserSync.stream());
 });
 
 gulp.task('connect', function() {
 	connect.server({
-		//настриваем конфиги сервера
-		root: [path.outputDir], //корневая директория запуска сервера
-		port: 9999, //какой порт будем использовать
-		livereload: true //инициализируем работу LiveReload
+		root: [path.outputDir],
+		port: 9999,
+		livereload: true
 	});
 });
 
 gulp.task('htmlMain:build', function() {
 	pump([
-		gulp.src(path.src.htmlMain), //Выберем файлы по нужному
-		gulp.dest(path.build.htmlMain), //выгрузим их в папку build
-		connect.reload() //И перезагрузим наш сервер для обновлений
+		gulp.src(path.src.htmlMain),
+		gulp.dest(path.build.htmlMain),
+		connect.reload()
 	]);
 });
 
 gulp.task('html:build', function() {
-	pump([
-		gulp.src(path.src.html), //Выберем файлы по нужному
-		gulp.dest(path.build.html), //выгрузим их в папку build
-		connect.reload() //И перезагрузим наш сервер для обновлений
-	]);
+	pump([gulp.src(path.src.html), gulp.dest(path.build.html), connect.reload()]);
 });
 
 gulp.task('jsMain:build', function() {
-	pump([
-		gulp.src(path.src.jsMain), //Найдем наш main файл
-		gulp.dest(path.build.jsMain) //выгрузим готовый файл в build
-	]);
+	pump([gulp.src(path.src.jsMain), gulp.dest(path.build.jsMain)]);
 });
 
 gulp.task('js:build', function() {
-	pump([
-		gulp.src(path.src.js), //Найдем наш main файл
-		gulp.dest(path.build.js) //выгрузим готовый файл в build
-	]);
+	pump([gulp.src(path.src.js), gulp.dest(path.build.js)]);
 });
 
 gulp.task('image:build', function() {
-	pump([
-		gulp.src(path.src.img),
-		gulp.dest(path.build.img) //выгрузим в build
-	]);
+	pump([gulp.src(path.src.img), gulp.dest(path.build.img)]);
 });
 
 gulp.task('css:build', function() {
 	pump([
-		gulp.src(path.src.scss), //Выберем наш основной файл стилей
-		sass().on('error', sass.logError), //Скомпилируем sass
-		gulp.dest(path.build.cssPreview), //вызгрузим в build
+		gulp.src(path.src.scss),
+		sass().on('error', sass.logError),
+		gulp.dest(path.build.cssPreview),
 		gulp.dest(path.build.css)
 	]);
 });
 
 gulp.task('sass', function() {
 	pump([
-		gulp.src(path.src.scss), //Выберем наш основной файл стилей
-		sass().on('error', sass.logError), //Скомпилируем sass
-		gulp.dest(path.build.cssPreview)
+		gulp.src(path.src.scss),
+		sass().on('error', sass.logError),
+		prefixer({
+			browsers: ['last 2 versions']
+		}),
+		gulp.dest(path.build.cssPreview),
+		browserSync.stream()
 	]);
 });
 
 gulp.task('cssmin', function() {
 	pump([
-		gulp.src(path.src.scss), //Выберем наш основной файл стилей
+		gulp.src(path.src.scss),
 		sass().on('error', sass.logError),
 		prefixer({
 			browsers: ['last 2 versions']
-		}), //Добавим вендорные префиксы
-		sourcemaps.init(), //инициализируем soucemap
-		cssmin(), //Сожмем
-		rename({ suffix: '.min' }), //добавим суффикс .min к имени выходного файла
-		sourcemaps.write('.'), //пропишем sourcemap
-		gulp.dest(path.build.css), //вызгрузим в build
+		}),
+		sourcemaps.init(),
+		cssmin(),
+		rename({ suffix: '.min' }),
+		sourcemaps.write('.'),
+		gulp.dest(path.build.css),
 		gulp.dest(path.build.cssPreview)
 	]);
 });
 
 gulp.task('fonts:build', function() {
-	pump([
-		gulp.src(path.src.fonts),
-		gulp.dest(path.build.fonts) //выгружаем в build
-	]);
+	pump([gulp.src(path.src.fonts), gulp.dest(path.build.fonts)]);
 });
 
 gulp.task('jsminMain', function(cb) {
@@ -214,7 +200,7 @@ gulp.task('imgmin', function() {
 				}
 			)
 		)
-		.pipe(gulp.dest(path.build.img)); //выгрузим в build
+		.pipe(gulp.dest(path.build.img));
 });
 
 gulp.task('build', [
@@ -231,9 +217,9 @@ gulp.task('build', [
 gulp.task('watch', ['connect'], function() {
 	gulp.watch(path.watch.scss, ['css:build'], connect.reload());
 	gulp.watch(path.watch.scssComponents, ['css:build'], connect.reload());
-	gulp.watch(path.watch.html, connect.reload()); // Наблюдение за HTML файлами в корне проекта
+	gulp.watch(path.watch.html, connect.reload());
 	gulp.watch(path.watch.htmlMain, connect.reload());
-	gulp.watch(path.watch.js, connect.reload()); // Наблюдение за JS файлами в папке js
+	gulp.watch(path.watch.js, connect.reload());
 	gulp.watch(path.watch.jsMain, connect.reload());
 });
 
