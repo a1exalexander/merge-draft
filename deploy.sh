03a310d74d16b712a7c02ac0d2e4a18ad90c9051
Alexander <forcewizu@gmail.com>
2018-10-30 00:36:24 +0200

    config

--- deploy.sh
+++ deploy.sh
@@ -19,5 +19,4 @@ git commit -m 'deploy'
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:a1exalexander/merge-draft.git master:gh-pages
 
-cd -
 
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	baseUrl: process.env.NODE_ENV === 'production' ? '/merge-draft/dist/' : '/'
+	baseUrl: process.env.NODE_ENV === 'production' ? '/merge-draft/' : '/'
 };
